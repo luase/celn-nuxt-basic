140a9807e1e8c7099531b65d7d3653b504ed0188
Jaime Alonso <a.lonso@yahoo.com>
2021-06-07 17:03:56 -0500

    nuxt-4

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="container mt-3">
-    <h1>detalle del articulo: {{ $route.params.id }}</h1>
+    <div class="card-body">
+      <h1>{{ articulo.title }}</h1>
+      <p>{{ articulo.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      articulo: ''
+    }
+  },
+  async created () {
+    try {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+      this.articulo = res.data
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e)
+    }
+  }
+}
 </script>
 
 <style scoped>
