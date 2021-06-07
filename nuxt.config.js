54b448805cf41b00d4e13886e629c248ba57b986
Jaime Alonso <a.lonso@yahoo.com>
2021-06-07 16:44:32 -0500

    nuxt-3

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,12 +11,15 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { href: 'https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2&display=swap', rel: 'stylesheet' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@assets/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,29 +2,35 @@
   <div class="container mt-3">
     <h1>blog</h1>
     <h2>lista de entradas</h2>
-    <div class="row">
-      <div v-for="(item, index) in articulos" :key="index" class="col-md-3">
-        <h2>
-          <nuxt-link :to="`/blog/${ item.id }`">
-            {{ item.titulo }}
-          </nuxt-link>
-        </h2>
+    <div v-for="(item, index) in articulos" :key="index" class="card mt-3">
+      <div class="card-body">
+        <nuxt-link :to="`blog/${item.id}`">
+          <h1>{{ item.title }}</h1>
+        </nuxt-link>
+        <p>{{ item.body }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
-      articulos: [
-        { id: 1, titulo: 'articulo 1' },
-        { id: 2, titulo: 'articulo 2' },
-        { id: 3, titulo: 'articulo 3' },
-        { id: 4, titulo: 'articulo 4' },
-        { id: 5, titulo: 'articulo 5' }
-      ]
+      articulos: []
+    }
+  },
+  head: {
+    title: 'blog'
+  },
+  async created () {
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      this.articulos = res.data
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.log(e)
     }
   }
 }
